858cd2bb2ef6d55e0d25e00d3cf653e86d465e79
Mauricio <mauricioasoto@gmail.com>
2023-10-30 19:36:16 -0300

    add email validation

--- src/Contato.java
+++ src/Contato.java
@@ -1,57 +1,84 @@
 public class Contato {
-    private int ID;
-    private String nome;
-    private String telefone;
-    private String email;
-    private String aniversario;
 
-    public Contato(int ID, String nome, String telefone) {
-        this.ID = ID;
-        this.setNome(nome);
-        this.setTelefone(telefone);
-    }
+  private int ID;
+  private String nome;
+  private String telefone;
+  private String email;
+  private String aniversario;
 
-    public int getID() {
-        return ID;
-    }
+  public Contato(int ID, String nome, String telefone) {
+    this.ID = this.setID();
+    this.setNome(nome);
+    this.setTelefone(telefone);
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public int getID() {
+    return ID;
+  }
 
-    public void setNome(String nome) {
-        if (nome != null && !nome.isEmpty()) {
-            this.nome = nome;
-        } else {
-            throw new IllegalArgumentException("Nome do contato é obrigatório.");
-        }
-    }
+  public void setID() {
+    Date date = new Date();
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    long milis = timestamp.getTime();
+    this.ID = (int) milis;
+  }
 
-    public String getTelefone() {
-        return telefone;
-    }
+  public String getNome() {
+    return this.nome;
+  }
 
-    public void setTelefone(String telefone) {
-        if (telefone != null && !telefone.isEmpty()) {
-            this.telefone = telefone;
-        } else {
-            throw new IllegalArgumentException("Telefone do contato é obrigatório.");
-        }
+  public void setNome(String nome) {
+    if (nome != null && !nome.isEmpty() && name.length() > 3) {
+      this.nome = nome;
+    } else {
+      throw new IllegalArgumentException("Nome do contato é obrigatório.");
     }
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getTelefone() {
+    return this.telefone;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
+  public void setTelefone(String telefone) {
+    if (telefone != null && !telefone.isEmpty() && phone.length() > 7) {
+      this.telefone = telefone;
+    } else {
+      throw new IllegalArgumentException("Telefone do contato é obrigatório.");
     }
+  }
 
-    public String getAniversario() {
-        return aniversario;
-    }
+  public String getEmail() {
+    return this.email;
+  }
 
-    public void setAniversario(String aniversario) {
-        this.aniversario = aniversario;
+  public static boolean isValidEmail(String email) {
+    String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
+    Pattern pattern = Pattern.compile(EMAIL_REGEX);
+    Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
+  }
+
+  public void setEmail(String email) {
+    if (isValidEmail(email)) {
+      this.email = email;
+    } else {
+      System.out.println("Email Invalido!");
     }
+  }
+
+  public String getAniversario() {
+    return this.aniversario;
+  }
+
+  public void setAniversario(String aniversario) {
+    this.aniversario = aniversario;
+  }
+
+  public void setName(String name) {
+    if (name != null && !name.isEmpty() && name.length() > 3) {
+      this.name = name;
+    } else {
+      System.out.println("Nome Invalido!");
+    }
+  }
 }
